487ede0d1cfadcb54666ca90c0f47874b995bab8
fadilian <fadilian1124@student.ub.ac.id>
2025-10-07 15:33:08 +0700

    update employee controller for new column (scheduleGroupId) and add changePassword function in auth controller

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
 import multer from "multer";
-import { register, loginAdmin,loginEmployee, profile, updateProfile, deleteAccount, requestPasswordReset, resetPassword, logout } from "../controllers/authController";
+import { 
+    register, 
+    loginAdmin,
+    loginEmployee, 
+    profile, 
+    updateProfile, 
+    deleteAccount, 
+    requestPasswordReset, 
+    resetPassword, 
+    logout,
+    changePasswordAdmin,
+    changePasswordEmployee
+ } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const upload = multer();
@@ -9,11 +21,15 @@ const router = Router();
 router.post("/register", upload.none(), register);
 router.post("/login/admin", loginAdmin);
 router.post("/login/employee", loginEmployee);
-router.get("/profile", authMiddleware, profile);
-router.patch("/profile/update", authMiddleware, updateProfile);
+router.get("/admin/profile", authMiddleware, profile);
+router.patch("/admin/profile/update", authMiddleware, updateProfile);
 router.delete("/profile/delete", authMiddleware, deleteAccount);
 router.post("/forgot-password", requestPasswordReset);
 router.post("/reset-password", resetPassword);
 router.post("/logout", authMiddleware, logout);
+// Admin/Superadmin ubah passwordnya sendiri
+router.patch("/admin/change-password", authMiddleware, changePasswordAdmin);
+// Employee ubah passwordnya sendiri
+router.patch("/employee/change-password", authMiddleware, changePasswordEmployee);
 
 export default router;
